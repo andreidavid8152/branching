f4ecb9e55c8d929d0187f310ff2676eefd2d2113
Andrei Flores <76012933+andreidavid8152@users.noreply.github.com>
2025-06-20 01:25:37 +0000

    Agregar logotipo

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main{
     public static void main(String[] args){
 
+        // Logotipo
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
+
         // Saludo
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Grupo"));
